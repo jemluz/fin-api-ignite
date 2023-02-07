9db900de0152816722db95bab6219673d48b2684
Jemima <contatojemima@gmail.com>
2023-02-07 11:46:51 -0300

    Utilizando os métodos HTTP

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ const { response } = require('express');
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "hello you ohh" })
+app.get("/courses", (req, res) => {
+  return res.json(["Curso 1", "Curso 2", "Curso 3"])
+})
+
+app.post("/courses", (req, res) => {
+  return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"])
+})
+
+app.put("/courses/:id", (req, res) => {
+  return res.json(["Curso 1.1", "Curso 2", "Curso 3", "Curso 4"])
+})
+
+app.patch("/courses/:id", (req, res) => {
+  return res.json(["Curso 1.1", "Curso 2", "Curso 3", "Curso 4"])
+})
+
+app.delete("/courses/:id", (req, res) => {
+  return res.json(["Curso 1.1", "Curso 2", "Curso 3", "Curso 4"])
 })
 
 app.listen(3333);
