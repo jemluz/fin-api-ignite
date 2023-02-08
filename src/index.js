043206676b9861a30967d10f207719f6fecd25ff
Jemima <contatojemima@gmail.com>
2023-02-08 18:17:58 -0300

    Passando parametros de diferentes tipos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-const { response } = require('express');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get("/courses", (req, res) => {
+  const query = req.query
+  console.log(query)
   return res.json(["Curso 1", "Curso 2", "Curso 3"])
 })
 
 app.post("/courses", (req, res) => {
+  const body = req.body
+  console.log(body)
   return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"])
 })
 
 app.put("/courses/:id", (req, res) => {
+  const params = req.params
+  console.log(params)
   return res.json(["Curso 1.1", "Curso 2", "Curso 3", "Curso 4"])
 })
 
