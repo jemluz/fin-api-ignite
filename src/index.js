02876b46052b75679b85a7d2b14d2a930dba760e
Jemima <contatojemima@gmail.com>
2023-02-08 19:43:46 -0300

    Validando CPF existente
    - create of a validation rule for existent cpf

--- src/index.js
+++ src/index.js
@@ -15,11 +15,21 @@ const costumers = [];
 
 app.post("/account", (req, res) => {
   const { name, cpf } = request.body;
-  const id = uuidv4();
+
+  const costumerAlreadyExists = costumers.some(
+    (costumer) => costumer.cpf === cpf
+  )
+
+  if(costumerAlreadyExists) {
+    return res.status(400).json({
+      error: "Costumer already exists!"
+    })
+  }
+
   costumers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: []
   });
 
